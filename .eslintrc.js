72d3abe401fadc341969cc4993daa18c28e68b63
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-07 10:53:22 +0000

    changes(chores): configuring the project

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['google'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
